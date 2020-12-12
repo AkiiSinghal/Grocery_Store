be913ca7258626ddfe676b5a6c8e4a9dd2cd476d
AkiiSinghal <akshatsinghal333@gmail.com>
2020-12-12 21:47:44 +0530

    add extra field and an admin user

---
+++ db/migrate/20201212132118_add_extra_fields_to_user.rb
@@ -0,0 +1,11 @@
+class AddExtraFieldsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string, null: false, default: ""
+    add_column :users, :last_name, :string, null: false, default: ""
+    add_column :users, :address, :text
+    add_column :users, :phone, :string
+    add_column :users, :user_type, :string
+    add_column :users, :shop, :string
+    add_column :users, :admin, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_113053) do
+ActiveRecord::Schema.define(version: 2020_12_12_132118) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_12_12_113053) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.text "address"
+    t.string "phone"
+    t.string "user_type"
+    t.string "shop"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
