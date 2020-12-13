ec73b7c888c69199fe5d144331443940eca774db
AkiiSinghal <akshatsinghal333@gmail.com>
2020-12-13 20:54:01 +0530

    add multiple image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
     belongs_to :user
+    #has_one_attached: image
+    has_many_attached :images
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+    validates :quantity, presence: true
+    validate :image_type
+
+    def thumbnail input
+        return self.images[input].variant(resize: '300x300!').processed
+    end
+
+    private
+    def image_type
+        if images.attached? == false
+            errors.add(:images, "are missing")
+        end
+        images.each do |image|
+            if !image.content_type.in?(%('image/jpeg image/png))
+                errors.add(:images, "need to be a JPEG or PNG")
+            end
+        end
+    end
 end
